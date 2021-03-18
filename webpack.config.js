972f23520f4cc3501ea5c4f9c307c31b070c9e98
Hiro Tanaka <ht@MacBook-Pro-ht.local>
2021-03-18 15:08:02 +0900

    change template

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,37 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// HTMLファイルのビルド設定
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+    template: path.join(__dirname, 'examples/src/index.html'),
+    filename: './index.html'
+});
 module.exports = {
-  mode: 'production',
-  entry: './src/YOUR_COMPONENT.js',
-  output: {
-    path: path.resolve('lib'),
-    filename: 'YOUR_COMPONENT.js',
-    libraryTarget: 'commonjs2',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        exclude: /(node_modules)/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
+    // 依存関係解決の起点となる資産を指定します。
+    entry: path.join(__dirname, 'examples/src/index.js'),
+    // Babelのトランスパイル対象資産を指定します。
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
         ]
-      }
-    ],
-  },
-  resolve: {
-    alias: {
-      'react': path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
-    }
-  },
-  externals: {
-    // Don't bundle react or react-dom      
-    react: {
-      commonjs: "react",
-      commonjs2: "react",
-      amd: "React",
-      root: "React"
     },
-    "react-dom": {
-      commonjs: "react-dom",
-      commonjs2: "react-dom",
-      amd: "ReactDOM",
-      root: "ReactDOM"
+    output: {
+        path: path.join(__dirname, "examples/dist"),
+        filename: "bundle.js"
+    },
+    plugins: [htmlWebpackPlugin],
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    // 開発用Webサーバのポートを指定します。
+    devServer: {
+        port: 3001
     }
-  }
-};
+}
